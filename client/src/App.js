f741a976c7ff8129d2b7e60ab9d376490a1b16ba
stevencharleshuang <steven.charles.huang@gmail.com>
2018-12-10 22:05:04 -0500

    created header and nav components with light styling, added nav-btn click handler

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import MKSketch from './img/mk-sketch.png';
 import './App.css';
+import Header from './components/Header';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navSelection: ''
+    };
+  }
+  handleNavClick = (e) => {
+    // console.log('Nav button clicked!', e.target.id);
+    this.setState(({
+      navSelection: e.target.id
+    }));
+  }
   render() {
+    console.log(this.state);
     return (
       <div className="App">
-        <h1>Hi Dave!</h1>
-        <img src={MKSketch} />
+        <Header handleNavClick={this.handleNavClick}/>
       </div>
     );
   }
