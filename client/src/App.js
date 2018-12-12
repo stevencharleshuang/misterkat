933a7e70e1a11ea1d89815deaa2ff2edf1f54408
stevencharleshuang <steven.charles.huang@gmail.com>
2018-12-12 14:56:55 -0500

    added core components to the front end, added react-router-dom, setup switch in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import About from './components/About';
+import Blogs from './components/Blogs';
+import Recipes from './components/Recipes';
 
 class App extends React.Component {
   constructor(props) {
@@ -20,6 +24,11 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header handleNavClick={this.handleNavClick}/>
+        <Switch>
+          <Route path="/misterkat-adventures" component={Blogs} />
+          <Route path="/meet-misterkat" component={About} />
+          <Route path="/katatatouille" component={Recipes} />
+        </Switch>
       </div>
     );
   }
