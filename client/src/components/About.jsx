664d7d358a47dd6698a52af094e085bad69d73db
stevencharleshuang <steven.charles.huang@gmail.com>
2018-12-20 18:24:40 -0500

    added content to the about component

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import ProfilePic from '../img/profile-001.jpg';
 
 export default function About() {
   return (
-    <h1>This is the About component</h1>
+    <div className="about">
+      <h1>Meet MisterKat</h1>
+      <div className="about-content">
+        <img src={ProfilePic} alt="Profile Picture"/>
+        <div className="text-content">
+          <p>
+            Lorem ipsum dolor sit amet, a risus eget pulvinar proin, lorem ut convallis vel duis. Nam ullam ligula at arcu cupidatat, lorem justo leo nulla, mattis ipsum metus vitae sapien, nisl mus fusce etiam. Et ligula. Porttitor non in turpis sit libero justo, cursus at massa. In lorem nulla rhoncus, pretium arcu torquent nec sapien. Vestibulum dui nascetur, purus augue arcu vestibulum, phasellus placerat donec mauris, molestie nonummy turpis tristique dapibus etiam, lobortis urna blandit non. Bibendum nibh ullamcorper id suspendisse sodales convallis, hac nunc feugiat. Tincidunt nunc viverra, et mi fermentum, magna auctor lectus phasellus blandit, ut ut eros bibendum placerat, id rem sit orci. Integer mi nunc luctus conubia, elementum in lectus, neque massa sollicitudin proin. A sem nisl convallis ac nunc. Erat sed consequat purus nisl dolor pellentesque, faucibus sed.
+          </p>
+          <p>
+            Odio per in nulla nunc urna vitae. Interdum dictum aliquam augue tellus justo eget, nulla egestas ullamcorper ultrices in dictum libero, ac magna augue ante sit curabitur lectus, eu ut porta in blandit lectus ut. Turpis ad odio interdum eu sed, fusce nulla elementum sed mauris wisi. A ligula in, sociis lectus montes bibendum proin ultrices. Urna fermentum, nec risus tincidunt, integer vel morbi nonummy. Mauris sed, nascetur suspendisse duis et in sed, maecenas vehicula, vehicula quis est quis convallis, etiam dignissim dolor tortor eget in. Nulla pellentesque, lacinia ligula et ante eu, quam gravida dictum, velit eu ipsum. Dis quis semper ridiculus urna semper, fusce ullamcorper praesent urna facilisis sodales, integer a in habitasse eu, vulputate ultrices eleifend enim tincidunt facilisis.
+          </p>            
+          <p>
+            Euismod egestas, vitae rutrum praesent eget nisl sit amet. Phasellus mollis nec sapien, ac dolor quam erat, pharetra mauris laborum adipiscing arcu mattis sollicitudin. Elementum est, ultrices massa eu commodo, aliquam montes ut lacus nibh, aliquet amet ultricies. Consequat magnis metus. A proin interdum suspendisse mauris, cursus phasellus tempus sed in lectus diam. Feugiat justo cursus placerat pellentesque amet accusamus, wisi pharetra, enim non sed velit vitae sed class, magna velit a dapibus id, eget risus a. Cras nulla aliquam, veritatis arcu ligula libero, ultricies vel, tristique metus donec hac, ante per sociosqu felis. Nec enim ut morbi. Ligula quis nec orci mauris ut amet, eros enim sodales vel integer semper, mus turpis nullam volutpat pede pede, eget etiam eu. Justo id. Sit sodales phasellus eu, at quis amet aenean iaculis, eleifend in ornare aenean, placerat litora tellus tincidunt nec in sit.
+          </p>
+          <p>
+            Vel sed placerat lacus sed, ac blandit. Etiam sed tellus nec quis sit nibh. Cum justo curabitur et metus etiam, metus amet odio eros consectetuer, consectetuer tincidunt ullamco aliquam, nec et praesent arcu cras. Rutrum feugiat, rutrum tellus tellus luctus ultrices. Nulla nisl ligula arcu at, erat aliquam dolor vel suspendisse sollicitudin, wisi libero pretium eius. Etiam inceptos semper dui, adipiscing lacus, et non adipiscing sapien senectus ridiculus. Pulvinar amet lorem at nunc, sem eget nisl facilisi, magna quisque ut lectus, nam risus et, consequat et quisque duis feugiat. Aliquam magna adipiscing pharetra turpis dui, convallis nunc vel facilisis orci, ac vitae ligula sed etiam magna, vivamus lobortis ultrices in quam magna. Vitae vel blandit suscipit felis libero consequat, ligula phasellus a elit fusce aenean nec, gravida ut molestie taciti integer nam. Eget vel pede leo at cursus, justo varius lobortis, auctor rhoncus luctus leo, condimentum ea amet massa.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Nav from './Nav';
-import MKSketch from '../img/mk-sketch.png';
+import LogoPic from '../img/mk-sketch.png';
 
 export default function Header(props) {
   return(
     <div className="header">
-      <img src={MKSketch} />
+      <img src={LogoPic} alt="Logo Picture" />
       <Nav handleNavClick={props.handleNavClick}/>
     </div>
   );
