7a16e58ad878032afaf4524f846852aa6f26c320
Steven Huang <steven.charles.huang@gmail.com>
2018-12-21 15:55:15 -0500

    added recipes gallery using hardcoded cdn images and light styling

--- client/src/components/Recipes.jsx
+++ client/src/components/Recipes.jsx
@@ -6,13 +6,24 @@ export default class Recipes extends React.Component {
     super(props);
     this.state = {
       recipesList: [],
+      recipesImages: [
+        'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA',
+        'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/mrtrending0475.jpg?itok=ULkGk3Pn',
+        'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/6046101_dtdin8546_beer.jpg?itok=SlXEw2dG',
+        'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/hanger-steak-lemon-butter.jpg?itok=RCk81X_O',
+        'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/mrtrending0470.jpg?itok=PdSpu3Af&1521836644',
+        'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/everything-jal-poppers.jpg?itok=cXIxgwiC',
+        'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/2016_10_18_myrecipes_2324.jpg?itok=5X5cKBA5',
+        'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/2487601_summe_0061.jpg?itok=IvXITcWe',
+        'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/steak-tomato-panzanella-ck.jpg?itok=GMpSDtcI',
+      ],
     }
   }
   render() {
     return (
       <div className="recipes">
         <h1>This is the Recipes component</h1>
-        <RecipesList />
+        <RecipesList recipesImages={this.state.recipesImages} />
       </div>
     );
   }
--- client/src/components/RecipesList.jsx
+++ client/src/components/RecipesList.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Recipe from './Recipe';
 
-export default function RecipesList() {
+export default function RecipesList(props) {
+  console.log(props);
+  let RecipesGallery = props.recipesImages.map((imageURL, i) => {
+    return <img src={imageURL} alt="imageURL" key="i" />     
+  })
   return (
     <div className="recipes-list">
       <h1>This is the RecipesList component</h1>
-      <Recipe />
+      <div className="recipes-gallery">
+        {RecipesGallery}
+      </div> 
+      {/* <Recipe /> */}
     </div>
   );
 }
