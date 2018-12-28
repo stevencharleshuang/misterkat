d2d891b903cf37c4e50977d35c0670d95ba5146e
Steven Huang <steven.charles.huang@gmail.com>
2018-12-27 22:48:25 -0500

    added styling to BlogsList component and logic for blog post text content excerpt

--- client/src/components/BlogsList.jsx
+++ client/src/components/BlogsList.jsx
@@ -4,10 +4,16 @@ import Blog from './Blog';
 export default function BlogsList(props) {
   let BlogsSelection = props.blogs.map((blog, i) => {
     return (
-      <img 
-        src={blog.imageURL}
-        key={i}
-      />
+      <article className="blog-post">
+        <img 
+          src={blog.imageURL}
+          id={blog.title}
+          key={i}
+        />
+        <div className="blog-excerpt">
+          <p>{blog.content.slice(0, 100).concat('...')}</p>
+        </div>
+      </article>
     );
   });
   return (
