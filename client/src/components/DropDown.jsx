8e806e5e3a669438f5993b4d1fb277e7a92b66b2
Steven Huang <steven.charles.huang@gmail.com>
2019-01-04 15:28:10 -0500

    added logic for dropdown functionality

--- client/src/components/DropDown.jsx
+++ client/src/components/DropDown.jsx
@@ -5,19 +5,54 @@ export default class DropDown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      displayDropDown: false,
     }
   }
+  showDropDown = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => ({
+      displayDropDown: !prevState.displayDropDown,
+    }));
+  }
+  hideDropDown = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => ({
+      // displayDropDown: false,
+      displayDropDown: !prevState.displayDropDown,
+    }));
+  }
+
   render() {
+    console.log('DropDown State', this.state);
     return(
-      <Link to="/misterkat-adventures">
+      // <Link to="/misterkat-adventures">
         <div 
           className="nav-btn" 
           id="misterkat-adventures" 
-          onClick={this.props.handleNavClick}>
+          // onClick={this.props.handleNavClick}
+          onClick={this.showDropDown}
+        >
           MisterKat Adventures
+          {
+            this.state.displayDropDown
+            ? <ul className="dropdown">
+              <li className="dropdown-item" id="local">
+                Local
+              </li>
+              <li className="dropdown-item" id="east-coast">
+                East Coast
+              </li>
+              <li className="dropdown-item" id="usa">
+                USA
+              </li>
+              <li className="dropdown-item" id="international">
+                International
+              </li>
+            </ul>
+            : null
+          }
         </div>
-      </Link>
+      // </Link>
     );
   }
 }
